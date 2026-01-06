71e0226cd85f56d0ec98cddea0f52995a36a11cc
Usama Javed <xcoderpc@gmail.com>
2026-01-07 02:59:47 +0500

    Initial commit

---
+++ Concepts/functions.js
@@ -0,0 +1,33 @@
+
+
+/// Function to add two numbers using arrow function syntax
+const addNumbers = (a, b) => {
+    return a + b;
+}
+
+/// Short version of function using arrow function syntax
+const add = (a, b) => a + b;
+
+/// Single parameter arrow function
+const greet = name => "Hello" + name;
+
+/// No parameter arrow function
+const hi = () => "Hi there!";
+
+const substract = (a, b) => a - b;
+
+const substractNumbers = (a, b) => {
+    return a - b;
+}
+
+const multiply = (a, b) => a * b;
+
+const multiplyNumbers = (a, b) => {
+    return a * b;
+}
+
+const divide = (a, b) => a / b;
+
+const divideNumbers = (a, b) => {
+    return a / b;
+}
---
+++ Concepts/variables.js
@@ -0,0 +1,15 @@
+
+
+/// Variable declaration
+let myAge = 25;
+
+// Constant declaration
+const myName = "Usama Javed";
+
+/// Simple way to write a function and pass a value to string interpolation
+function introduce(){
+    myAge = 28;
+    console.log(`Hello, my name is ${myName} and I am ${myAge} years old.`);
+}
+
+introduce();
